3e3359177fc7fcffdfb04648ce9f69289f5e997a
Joel Deon Dsouza <oka.welt.dev@gmail.com>
2022-10-02 12:42:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/todolist/ToDoAdoptor.kt
@@ -0,0 +1,40 @@
+package com.example.todolist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+
+
+
+
+class ToDoAdoptor(
+    private val todos: MutableList<ToDo>
+) : RecyclerView.Adapter<ToDoAdoptor.ToDoViewHolder>() {
+
+  class ToDoViewHolder(itemView:View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoViewHolder {
+        return ToDoViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_todo,
+                parent,
+//                attachToRoot:false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ToDoViewHolder, position: Int) {
+        val curToDo = todos[position]
+        holder.itemView.apply {
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return todos.size
+    }
+
+
+}
